6ccb8c4de21e87a2d8602b1d4d91d7b96f22ba04
sahil <sahil.saxena@student.manchester.ac.uk>
2024-06-26 12:35:13 +0100

    Light/dark mode toggle generated

--- script.js
+++ script.js
@@ -59,12 +59,47 @@ function operate(a, b, op){
         text_box.textContent = display_content;
     }
     operator = "";
-    point_count = 0;
+    if (display_content.includes('.')){
+        point_count = 1;
+    }else{
+        point_count = 0;
+    }
+
 }
 
+function handleKeyEvent(event){
+    let key = event.key;
+    let keyMappings = {
+        'Escape': 'AC',
+        '+': '+',
+        '-': '-',
+        '*': '*',
+        '/': '/',
+        'Enter': '=',
+        '=': '=',
+        '1': '1',
+        '2': '2',
+        '3': '3',
+        '4': '4',
+        '5': '5',
+        '6': '6',
+        '7': '7',
+        '8': '8',
+        '9': '9',
+        '0': '0',
+        '.': '.',
+        '%': '%'
+    };
+    if (key in keyMappings){
+        let text = keyMappings[key];
+        console.log(text);
+        populate(text);
+    }
 
-function populate(event){
-    let text = event.target.textContent;
+}
+
+function populate(input){
+    let text = input.toString();
     if (text === 'AC'){
         display_content = "";
         text_box.textContent = display_content;
@@ -79,14 +114,14 @@ function populate(event){
         if (operator !== ""){
             const arr = display_content.split(operator);
             if (!(arr[0] === ""||arr[1] === "")){
-                num1 = arr[0];
-                num2 = arr[1];
+                num1 = parseFloat(arr[0]);
+                num2 = parseFloat(arr[1]);
             }
             operate(num1, num2, operator);
         }        
     }else if(text === '+/-'){
         if (operator === ""){
-            display_content = display_content * -1;
+            display_content = parseFloat(display_content) * -1;
             text_box.textContent = display_content;
         }     
     }else if (text === '%'){
@@ -108,6 +143,12 @@ function populate(event){
         display_content = display_content + text;
         text_box.textContent = display_content;
     }
+}
+
+
+function populateButton(event){
+    let text = event.target.textContent;
+    populate(text);
     
 }
 
@@ -120,6 +161,8 @@ let point_count = 0;
 let display_content = "";
 
 number_buttons.forEach(function(number){
-    number.addEventListener('click', populate);
+    number.addEventListener('click', populateButton);
 });
+
+document.addEventListener('keydown',handleKeyEvent);
 
